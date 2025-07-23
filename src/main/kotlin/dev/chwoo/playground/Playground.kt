4cd1155b3d5b13d3c5aeb65ab210559bac6a3833
Hyun <koreahan8549@gmail.com>
2025-07-23 17:27:30 +0900

    Initial commit

---
+++ src/main/kotlin/dev/chwoo/playground/Playground.kt
@@ -0,0 +1,14 @@
+package dev.chwoo.playground
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Playground : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
